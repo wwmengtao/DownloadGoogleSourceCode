92af4c36a23e2a314e8eb5a8add34322151764bf
wwmengtao <wwmengtao@gmail.com>
2017-06-07 16:40:26 +0800

    修改
    Signed-off-by: wwmengtao <wwmengtao@gmail.com>

--- download-src.py
+++ download-src.py
@@ -13,18 +13,21 @@ def isSuitableBranch(pathName, branchNames):
 	
 
 #Downloads All Android SourceCode
-def DownloadAll(rootdir_ori, defaultXml, prefix, branchNames, suffix):
+def DownloadAll(rootdir_ori, defaultXml, branchNames):
 	dom = xml.dom.minidom.parse(defaultXml)  
 	root = dom.documentElement  
+	AndroidVersion=""
 	for node in root.getElementsByTagName("default"):
 		AndroidVersion=node.getAttribute("revision").replace("refs/tags/", "")
 		break;
-	print AndroidVersion
+	#print AndroidVersion
 	rootdir=rootdir_ori+"/"+AndroidVersion
 	if not os.path.exists(rootdir):  
 		os.makedirs(rootdir)  
 	os.chdir(rootdir)#Change current work dir
-
+	#Download commands
+	prefix = "git clone https://android.googlesource.com/"  
+	suffix = ".git"  
 	for node in root.getElementsByTagName("project"):  
 		d = node.getAttribute("path") 
 		if True != isSuitableBranch(d, branchNames):	
@@ -40,23 +43,18 @@ def DownloadAll(rootdir_ori, defaultXml, prefix, branchNames, suffix):
 		call(cmd)
 
 #Following are certain configurations:
-#1.git program path  
-git = "D:/Software/PortableGit-1.8.4-preview20130916_dinit_rev6/PortableGit-1.8.4-preview20130916/bin/git.exe" 
+#1.Downloaded source path  
+SourceCodeRootDir = "D:/Android/SourceCode"
 #2.manifestDir
-manifestDir = "E:/Android/SourceCode/manifest";
+manifestDir = SourceCodeRootDir+"/"+"manifest";
 defaultXml=manifestDir+"/"+"default.xml"
-#3.Downloaded source path  
-SourceCodeRootDir = "E:/Android/SourceCode/AndroidN"
-#4.Download commands
-prefix = "git clone https://android.googlesource.com/"  
-suffix = ".git"  
-
-#5.Start to download:
+#3.branchNames
 branchNames=[
 "packages",
 "frameworks/base",
 "frameworks/opt",
 #"frameworks/volley"
 ]
-DownloadAll(SourceCodeRootDir, defaultXml, prefix, branchNames, suffix)
-#Notes:1)The python file is only for Windows;2)About detailed info, refer to YinXiang note
+#4.Start to download:
+DownloadAll(SourceCodeRootDir, defaultXml, branchNames)
+#Notes:1)The python file is only for Windows;2)About detailed info, refer to Weizhi note
